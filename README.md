1282f2203d696eb26ae7b3addc2ebbc822344dcb
Replit user <>
2023-08-27 17:37:05 +0000

    full project

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# Timestamp Microservice
+# FCC - Timestamp Microservice
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Express](https://img.shields.io/badge/Express%20js-000000?style=for-the-badge&logo=express&logoColor=white)
+
+Live demo of **[Timestamp Microservice](https://boilerplate-project-timestamp.odakris.repl.co/)**
+
+## Description
 
 This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+
+This project is part of the **[freeCodeCamp](https://www.freecodecamp.org/learn/back-end-development-and-apis)** Back End Development and APIs projects certification.
+
+<p align="center">
+  <img src="./images/timestamp.png">
+</p>
+
+## Instructions
+
+Clone this repository and install the NPM packages:
+
+```
+npm install
+```
+
+In the project directory, run the following script:
+
+```
+npm start
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-// index.js
-// where your node app starts
-
 // init project
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// API endpoint
+app.get("/:key?", (req, res, next) => {
+  let key = req.params.key;
+
+  key === "api" 
+    ? next() 
+    : res.json({
+      "error 404": "please use correct route => [url]/api"
+    })
+});
+
+app.get("/api/:date?", (req, res) => {
+  let inputDate = req.params.date;
+  let utcDate;
+  let isNumRegex = /^[0-9]+$/;
+  console.log(req.software);
+
+  if (!inputDate) {
+    // if not input date, get current date
+    utcDate = new Date();
+  } else {
+    isNumRegex.test(inputDate)
+      ? // if input date is numerical (unix format)
+      (utcDate = new Date(parseInt(inputDate)))
+      : // else input date is date format
+      (utcDate = new Date(inputDate));
+  }
+
+  // check for valid date or not
+  utcDate.toUTCString() === "Invalid Date"
+    ? res.json({ error: utcDate.toUTCString() })
+    : res.json({ unix: utcDate.getTime(), utc: utcDate.toUTCString() });
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+// listen for requests
+const listener = app.listen(port, function() {
+  console.log("Your app is listening on port " + listener.address().port);
 });
